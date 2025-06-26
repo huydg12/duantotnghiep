48f8007a20bf2c2a1bd14c0e648e05a3bfd8ccf5
huydg12 <kun12112002@gmail.com>
2025-06-26 18:20:27 +0700

    Add source BE

---
+++ BE_main/src/main/java/com/poly/BE_main/BeMainApplication.java
@@ -0,0 +1,13 @@
+package com.poly.BE_main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeMainApplication.class, args);
+	}
+
+}
---
+++ BE_main/src/test/java/com/poly/BE_main/BeMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.BE_main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
