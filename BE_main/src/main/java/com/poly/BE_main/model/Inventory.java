d0f6fa4b62af8d7c96724731c9b584a6d0555dec
khangbt3105205 <trongkhang0503@gmail.com>
2025-07-01 00:34:37 +0700

    UPDATE INVENTORY

--- BE_main/src/main/java/com/poly/BE_main/model/Inventory.java
+++ BE_main/src/main/java/com/poly/BE_main/model/Inventory.java
@@ -1,5 +1,74 @@
 package com.poly.BE_main.model;
 
+import java.time.LocalDateTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "INVENTORY")
 public class Inventory {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private int id;
+
+    @Column(name = "PRODUCT_DETAIL_ID")
+    private int productDetailId;
+
+    @Column(name = "QUANTITY")
+    private int quantity;
+
+    @Column(name = "MODIFIED_DATE")
+    private LocalDateTime modifiedDate;
+
+    public Inventory() {
+    }
+
+    public Inventory(int id, int productDetailId, int quantity, LocalDateTime modifiedDate) {
+        this.id = id;
+        this.productDetailId = productDetailId;
+        this.quantity = quantity;
+        this.modifiedDate = modifiedDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProductDetailId() {
+        return productDetailId;
+    }
+
+    public void setProductDetailId(int productDetailId) {
+        this.productDetailId = productDetailId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public LocalDateTime getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(LocalDateTime modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+    
+
+    
 
 }
---
+++ BE_main/src/main/java/com/poly/BE_main/service/InventoryService.java
@@ -0,0 +1,27 @@
+package com.poly.BE_main.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.poly.BE_main.model.Inventory;
+import com.poly.BE_main.repository.InventoryRepository;
+
+@Service
+
+public class InventoryService {
+    @Autowired
+    InventoryRepository inventoryRepository;
+
+    public List<Inventory> findAll(){
+        return inventoryRepository.findAll();
+    }
+
+    public Inventory update(int id, Inventory iUpdate){
+        return inventoryRepository.findById(id).map(i -> {
+            i.setQuantity(iUpdate.getQuantity());
+            i.setModifiedDate(iUpdate.getModifiedDate());
+            return inventoryRepository.save(i);
+        }).orElseThrow(()-> new RuntimeException( "Không tìm thấy kho có id: " + id));
+    }
+
+}
