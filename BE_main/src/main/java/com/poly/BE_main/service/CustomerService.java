0731c6bde385c772274439f912c289d469acf289
huyphammmm <huypham170903@gmail.com>
2025-07-01 23:10:36 +0700

    update custom tétttétt

--- BE_main/src/main/java/com/poly/BE_main/service/CustomerService.java
+++ BE_main/src/main/java/com/poly/BE_main/service/CustomerService.java
@@ -4,8 +4,5 @@ import java.util.List;
 import com.poly.BE_main.model.Customer;
 
 public interface CustomerService {
-    List<Customer> findAll();
-    Customer findById(Long id);
-    Customer save(Customer customer);
-    void deleteById(Long id);
+
 }
